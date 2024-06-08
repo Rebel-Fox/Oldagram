8a26e4955dca10abf197df0db1ef43b32bbb72fa
Rebel-Fox <rookiewebdev@outlook.com>
2024-06-08 13:32:30 +0530

    modified js file

--- index.js
+++ index.js
@@ -28,3 +28,59 @@ const posts = [
     }
 ]
 
+const main=document.getElementById("main")
+ 
+for(let i=0;i<posts.length;i++){
+    main.innerHTML +=`<section>
+            <div class="credentials container">
+                <img src= ${posts[i].avatar} alt="${posts[i].name} profile picture" class="avatar" />
+                <h3>${posts[i].name}</h3>
+                <p class="location">${posts[i].location}</p>
+            </div>
+        </section>
+
+        <section>
+            <div class="post container">
+                <img src=${posts[i].post} alt="${posts[i].name} Potrait" class="postcontent" />
+            </div>
+        </section>
+
+        <section>
+            <div class="interaction container">
+                <button class="icon" id="like-btn-${i}"><img src="images/icon-heart.png" alt="heart icon for liking" ></button>
+                <button class="icon"><img src="images/icon-comment.png" alt="comment icon" /></button>
+                <button class="icon"><img src="images/icon-dm.png" alt="aeroplane shape share icon"/></button>
+            </div>
+        </section>
+
+        <section>
+            <div class="output container">
+                <p class="likes"><span id="likes-cnt-${i}">${posts[i].likes}</span> likes</p>
+                <p class="last-para"><span class="username">${posts[i].username}</span><span class="comment"> ${posts[i].comment}</span></p>
+            </div>
+        </section>`
+}
+
+// Attach event listeners
+for (let i = 0; i < posts.length; i++) {
+    const likeBtn = document.getElementById(`like-btn-${i}`)
+    const likesCnt = document.getElementById(`likes-cnt-${i}`)
+    let isLike=false
+    likeBtn.addEventListener("click", function() {
+        
+        if(!isLike){
+            posts[i].likes++
+            likesCnt.innerHTML = posts[i].likes 
+            isLike =true
+        } else{
+            posts[i].likes--
+            likesCnt.innerHTML = posts[i].likes
+            isLike = false
+        }
+    })
+}
+
+
+
+
+
